96c1d2b978686ba2fcbacb598c8551e98f4a2e63
beginerbeginer <miyabi6125@gmail.com>
2021-09-28 15:20:36 +0900

    add: ecs task's security group

--- security_group.tf
+++ security_group.tf
@@ -63,3 +63,23 @@ resource "aws_lb_listener" "hello_world" {
     Name = "aws_lb_listner_helloworld"
   }
 }
+
+# ecs taskのセキュリティーグループ
+resource "aws_security_group" "hello_world_task" {
+  name        = "example-task-security-group"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 3000
+    to_port         = 3000
+    security_groups = [aws_security_group.lb.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
