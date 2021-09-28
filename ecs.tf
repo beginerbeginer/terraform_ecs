c20a890cc27590622bf1452c66d4fff2250bcfd8
beginerbeginer <miyabi6125@gmail.com>
2021-09-28 16:40:40 +0900

    add:ecs

--- ecs.tf
+++ ecs.tf
@@ -23,3 +23,31 @@ resource "aws_ecs_task_definition" "hello_world" {
 ]
 DEFINITION
 }
+
+resource "aws_ecs_cluster" "main" {
+  name = "example-cluster"
+}
+
+resource "aws_ecs_service" "hello_world" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.hello_world.arn
+  desired_count   = var.app_count
+  launch_type     = "FARGATE"
+
+  # タスクが実行される場所
+  network_configuration {
+    security_groups = [aws_security_group.hello_world_task.id]
+    subnets         = aws_subnet.private.*.id
+  }
+
+  # load_balancerブロックで定義されたロードバランサーを介して外界から到達可能
+  load_balancer {
+    target_group_arn = aws_lb_target_group.hello_world.id
+    container_name   = "hello-world-app"
+    container_port   = 3000
+  }
+
+  # ロードバランサーが動作するまでサービスを作成してはいけないので、ロードバランサーのリスナーをdepends_onの配列に含める
+  depends_on = [aws_lb_listener.hello_world]
+}
