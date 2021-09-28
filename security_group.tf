251130a3ddc6ce062fdd2c4fda11a8c1dab75e10
beginerbeginer <miyabi6125@gmail.com>
2021-09-28 15:10:57 +0900

    add:load balancer

--- security_group.tf
+++ security_group.tf
@@ -23,3 +23,43 @@ resource "aws_security_group" "lb" {
     Name = "sg-for-ecs"
   }
 }
+
+# -----------------------
+# Load Balancer
+# -----------------------
+resource "aws_lb" "default" {
+  name            = "example-lb"
+  subnets         = aws_subnet.public.*.id
+  security_groups = [aws_security_group.lb.id]
+
+  tags = {
+    Name = "aws_lb-default"
+  }
+}
+
+resource "aws_lb_target_group" "hello_world" {
+  name        = "example-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.default.id
+  target_type = "ip"
+
+  tags = {
+    Name = "aws_lb_group_helloworld"
+  }
+}
+
+resource "aws_lb_listener" "hello_world" {
+  load_balancer_arn = aws_lb.default.id
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.hello_world.id
+    type             = "forward"
+  }
+
+  tags = {
+    Name = "aws_lb_listner_helloworld"
+  }
+}
